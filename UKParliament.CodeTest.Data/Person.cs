78603b224fb306d166e9aeebe90edfa2497154d2
Rob Ryan <157363455+rryan1988@users.noreply.github.com>
2025-03-07 20:01:47 +0000

    Initial Commit

---
+++ UKParliament.CodeTest.Data/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace UKParliament.CodeTest.Data;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+    public string Department { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
